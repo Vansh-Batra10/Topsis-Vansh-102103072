42bf0116b5be7326798b533cc85cc147a7ada415
unknown <batravansh162@gmail.com>
2024-01-22 03:48:08 +0530

    lastlast

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'Topsis-Vansh-102103072',          
   packages = ['Topsis-Vansh-102103072'],  
-  version = '0.0.1',     
+  version = '1.0',     
   license='MIT',    
   description = 'This is a library for implementing Topsis in command line',
   long_description=README,
@@ -18,7 +18,7 @@ setup(
   author = 'Vansh Batra',                   
   author_email = 'batravansh162@gmail.com',      
   url = 'https://github.com/Vansh-Batra10/Topsis-Vansh-102103072',   
-  download_url = 'https://github.com/Vansh-Batra10/Topsis-Vansh-102103072/archive/refs/tags/v1.0.0.tar.gz ',    
+  download_url = 'https://github.com/Vansh-Batra10/Topsis-Vansh-102103072/archive/refs/tags/v1.0.tar.gz',    
   keywords = ['TOPSIS', 'Command Line'],   
   install_requires=[            
           'numpy',
@@ -42,7 +42,7 @@ setup(
   ],
   entry_points={
         "console_scripts": [
-            "topsis=102103072:__main__",
+            "topsis=Topsis-Vansh-102103072.102103072:__main__",
         ]
     },
 )
